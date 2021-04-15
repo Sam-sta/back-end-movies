f7fbd7933348b7940dbf8d649a358cce84379b96
Sam-sta <sasanchez3010@gmail.com>
2021-04-14 19:58:30 -0500

    Proyecto backend finalizado ultimas actualizaciones

--- src/main/java/co/edu/iudigital/app/model/repository/UserRepository.java
+++ src/main/java/co/edu/iudigital/app/model/repository/UserRepository.java
@@ -6,4 +6,5 @@ import co.edu.iudigital.app.model.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
 
+	public User findByEmailAndPassword(String email, String password);
 }
